f2e1a3afdd5caac9eeab13201588b92ada36ba33
huzhouyu <huzhouyu@eatown.com.cn>
2019-12-30 10:38:35 +0800

    2

--- main.go
+++ main.go
@@ -1,21 +1,19 @@
 package main
 
 import (
-	"github.com/freefishgo/freefishgo/middlewares/mvc"
-
-	"github.com/freefishgo/freefishgo"
-	"github.com/freefishgo/freefishgo/middlewares/printTimeMiddleware"
 	_ "freefishgodoc/conf"
 	_ "freefishgodoc/controllers"
 	_ "freefishgodoc/routers"
-
+	"github.com/freefishgo/freefishgo"
+	"github.com/freefishgo/freefishgo/middlewares/mvc"
+	"github.com/freefishgo/freefishgo/middlewares/printTimeMiddleware"
 )
 
 func main() {
 	// 通过注册中间件来打印任务处理时间服务
 	freefishgo.UseMiddleware(&printTimeMiddleware.PrintTimeMiddleware{})
 	// 利用中间件来实现http到https的转换
-	//conf.Build.UseMiddleware(&httpToHttps.HttpToHttps{})
+	//freefishgo.UseMiddleware(&httpToHttps.HttpToHttps{})
 	// 把mvc实例注册到管道中
 	freefishgo.UseMiddleware(mvc.DefaultMvcApp)
 	freefishgo.DefaultConfig.Listen.HTTPPort = 8080
