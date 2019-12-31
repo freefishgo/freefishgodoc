1c56f952d0ff12652f374d225c8f03555112e74d
huzhouyu <huzhouyu@eatown.com.cn>
2019-12-31 16:54:55 +0800

    2

--- views/froala/Index.fish
+++ views/froala/Index.fish
@@ -9,37 +9,21 @@
 <link href="/static/css/froala_editor.pkgd.min.css" rel="stylesheet" type="text/css" />
 <!-- Create a tag that we will use as the editable area. -->
 <!-- You can use a div tag as well. -->
-<textarea></textarea>
-
-
+<textarea id="edit"></textarea>
+<div id="edit1"></div>
+<button id="save">获取代码</button>
 <!-- Include Editor JS files. -->
 <script src="/static/js/jquery-3.3.1.min.js"></script>
 <script type="text/javascript" src="/static/js/froala_editor.pkgd.min.js"></script>
 
 <!-- Initialize the editor. -->
 <script>
-    f=new FroalaEditor('textarea');
-
     $(function() {
-        // var editCont = '<p>adddddp</p>';
-        // $.FroalaEditor.DefineIcon('color', {SRC: 'txt_color@2x.png', ALT: 'Image button', template: 'image'}); //自定义图标
-        // $('#edit').froalaEditor({
-        //     autofocus: true,
-        //     toolbarButtonsXS: ['undo', 'redo', '|', 'bold', 'italic', 'underline', '|', 'fontSize', 'align', 'color'],
-        //     language: 'zh_cn',
-        //     colorsHEXInput: false,
-        //     colorsBackground: ['#2E2E2E', '#767676', '#DF281B', '#F4821C', '#46AC43', '#2E5BF7', '#A249B3', 'REMOVE'],
-        //     colorsText: ['#2E2E2E', '#767676', '#DF281B', '#F4821C', '#46AC43', '#2E5BF7', '#A249B3', 'REMOVE'],
-        //     fontSize: ['14', '16', '18', '20'],
-        //     fontSizeDefaultSelection: '16',
-        //     height: 220,
-        //     htmlAllowComments: false,
-        //     pasteAllowedStyleProps: ['font-size', 'color'],
-        //     placeholderText: '请输入内容',
-        //     charCounterMax: 500
-        // }).froalaEditor('html.set', editCont);
+        editor=new FroalaEditor('#edit');
+
         $("#save").click(function(){
-            $('#edit').froalaEditor('html.get', true); //返回富文本编辑里面的html代码
+            $("#edit1").html(editor.html.get())
+            console.log(editor.html.get())
         })
     });
 
