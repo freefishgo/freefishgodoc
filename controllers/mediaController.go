6210c876f1df843e46c46355751892c42b004747
huzhouyu <a1085052074@qq.com>
2020-01-12 18:48:32 +0800

    增加文档上传接口
    Signed-off-by: huzhouyu <a1085052074@qq.com>

---
+++ controllers/mediaController.go
@@ -0,0 +1,77 @@
+package controllers
+
+import (
+	"freefishgodoc/tools"
+	"image/jpeg"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/freefishgo/freefishgo/middlewares/mvc"
+	"github.com/nfnt/resize"
+)
+
+type mediaController struct {
+	mvc.Controller
+}
+
+func init() {
+	mvc.AddHandlers(&mediaController{})
+}
+
+func (media *mediaController) UploadFilePost() {
+	resp := &struct {
+		Link string `json:"link"`
+	}{}
+	f, fh, err := media.Request.FormFile("file")
+	defer f.Close()
+	if err != nil {
+		resp.Link = ""
+		return
+	}
+	ty := filepath.Ext(fh.Filename)
+	path := filepath.Join("/uploadfile", strings.ToLower(strings.Trim(ty, ".")), tools.GetGuid()+ty)
+	resp.Link = path
+	media.Response.WriteJson(resp)
+	path = filepath.Join("static", path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if media.Query["type"] == "img" {
+		// decode jpeg into image.Image
+		img, err := jpeg.Decode(f)
+		if err != nil {
+			return
+		}
+		// resize to width 1000 using Lanczos resampling
+		// and preserve aspect ratio
+		m := resize.Resize(1000, 0, img, resize.Lanczos3)
+
+		out, err := os.Create(path)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer out.Close()
+
+		// write new image to file
+		jpeg.Encode(out, m, nil)
+		return
+	}
+	tools.WriteFileFromReader(path, f)
+}
+
+// 控制器执行前调用
+func (media *mediaController) Prepare() {
+	userinfo := media.Response.GetSession("userinfo")
+	userip := media.Response.GetSession("userip")
+	if userinfo != nil && userip != nil {
+		if userip != media.Request.Host {
+			media.Response.Write([]byte("违规操作"))
+			media.SkipController()
+		}
+	} else {
+		media.Response.Write([]byte("登录过期了"))
+		media.SkipController()
+	}
+}
