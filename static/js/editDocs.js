a026c6945571654e2cdfca3e0055e3329e71e555
huzhouyu <huzhouyu@eatown.com.cn>
2020-01-16 13:34:05 +0800

    1
    Signed-off-by: huzhouyu <huzhouyu@eatown.com.cn>

--- static/js/editDocs.js
+++ static/js/editDocs.js
@@ -21,8 +21,23 @@ var froalaOption ={
     quickInsertTags: [],
 
     // toolbar buttons
-    // toolbarButtons: ['fullscreen', 'bold', 'italic', 'underline', 'strikeThrough', '|', 'paragraphFormat', 'fontSize', 'color', '|', 'align', 'formatOL', 'formatUL', 'outdent', 'indent', 'quote', '-', 'insertLink', 'insertFile', 'insertImage', 'insertVideo', 'embedly', 'insertTable', '|', 'insertHR', 'selectAll', 'clearFormatting', '|', 'spellChecker', 'help', 'html', '|', 'undo', 'redo'],
-
+    //toolbarButtons: ['fullscreen', 'bold', 'italic', 'underline', 'strikeThrough', '|', 'paragraphFormat', 'fontSize', 'color', '|', 'align', 'formatOL', 'formatUL', 'outdent', 'indent', 'quote', '-', 'insertLink', 'insertFile', 'insertImage', 'insertVideo', 'embedly', 'insertTable', '|', 'insertHR', 'selectAll', 'clearFormatting', '|', 'spellChecker', 'help', 'html', '|', 'undo', 'redo'],
+    // toolbarButtons: {
+    //     'moreText': {
+    //       'buttons': ['bold', 'italic', 'underline', 'strikeThrough', 'subscript', 'superscript', 'fontFamily', 'fontSize', 'textColor', 'backgroundColor', 'inlineClass', 'inlineStyle', 'clearFormatting']
+    //     },
+    //     'moreParagraph': {
+    //       'buttons': ['alignLeft', 'alignCenter', 'formatOLSimple', 'alignRight', 'alignJustify', 'formatOL', 'formatUL', 'paragraphFormat', 'paragraphStyle', 'lineHeight', 'outdent', 'indent', 'quote']
+    //     },
+    //     'moreRich': {
+    //       'buttons': ['insertLink', 'insertImage', 'insertVideo', 'insertTable', 'emoticons', 'fontAwesome', 'specialCharacters', 'embedly', 'insertFile', 'insertHR']
+    //     },
+    //     'moreMisc': {
+    //       'buttons': ['undo', 'redo', 'fullscreen', 'print', 'getPDF', 'spellChecker', 'selectAll', 'html', 'help'],
+    //       'align': 'right',
+    //       'buttonsVisible': 2
+    //     }
+    //   },
     // upload file
     fileUploadParam: 'file',
     fileUploadURL: '/media/UploadFile',
